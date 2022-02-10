c5ae565d9ffc0448fc54be64ee768c1904684e10
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-10 13:19:23 +0100

    Added Dimensions

--- app/src/main/java/com/decline/noteapp/ui/theme/Theme.kt
+++ app/src/main/java/com/decline/noteapp/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -28,17 +29,21 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun NoteAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun NoteAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
