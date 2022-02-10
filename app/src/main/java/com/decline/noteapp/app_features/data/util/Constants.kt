e52182d39636d85912b744b969c497c8f7e02e06
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-10 20:15:52 +0100

    Added colors

--- app/src/main/java/com/decline/noteapp/app_features/data/util/Constants.kt
+++ app/src/main/java/com/decline/noteapp/app_features/data/util/Constants.kt
@@ -1,6 +1,10 @@
 package com.decline.noteapp.app_features.data.util
 
+import com.decline.noteapp.ui.theme.*
+
 object Constants {
 
     const val DATABASE_NAME = "notes_db"
+
+     val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
 }
