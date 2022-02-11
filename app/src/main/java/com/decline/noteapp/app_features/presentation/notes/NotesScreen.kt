84596c5667d843b2bff0b6e12529e5888c9c457c
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-11 12:34:50 +0100

    Added BottomAppBar and Updated FAB

--- app/src/main/java/com/decline/noteapp/app_features/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/decline/noteapp/app_features/presentation/notes/NotesScreen.kt
@@ -1,17 +1,23 @@
 package com.decline.noteapp.app_features.presentation.notes
 
+import android.graphics.drawable.shapes.Shape
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -34,12 +40,23 @@ fun NotesScreen(
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
+                shape = RoundedCornerShape(spacing.spaceMedium),
                 onClick = {
                     navController.navigate(Screen.AddEditNoteScreen.route)
                 },
                 backgroundColor = MaterialTheme.colors.primary
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
+            }
+        },
+        isFloatingActionButtonDocked = true,
+        bottomBar = {
+            BottomAppBar(
+                cutoutShape = MaterialTheme.shapes.small.copy(
+                    CornerSize(percent = 35)
+                )
+            ) {
+
             }
         },
         scaffoldState = scaffoldState
