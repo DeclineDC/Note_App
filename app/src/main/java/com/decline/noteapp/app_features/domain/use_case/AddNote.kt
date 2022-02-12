8c2e98279c7281991e507c553bf21ad89a911aff
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-12 13:43:19 +0100

    Added InvalidNote handling

--- app/src/main/java/com/decline/noteapp/app_features/domain/use_case/AddNote.kt
+++ app/src/main/java/com/decline/noteapp/app_features/domain/use_case/AddNote.kt
@@ -2,13 +2,20 @@ package com.decline.noteapp.app_features.domain.use_case
 
 import com.decline.noteapp.app_features.domain.model.Note
 import com.decline.noteapp.app_features.domain.repository.NoteRepository
+import com.decline.noteapp.app_features.domain.util.InvalidNoteException
 
 class AddNote(
     private val repository: NoteRepository
 ) {
 
-
+    @Throws(InvalidNoteException::class)
     suspend operator fun invoke(note: Note) {
+        if (note.title.isBlank()) {
+            throw InvalidNoteException("The title of the note can't be empty.")
+        }
+        if (note.content.isBlank()) {
+            throw InvalidNoteException("The content of the note can't be empty.")
+        }
         repository.insertNote(note)
     }
 }
