92b78d16d61279325fd66af756abae606b7b913f
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-12 15:03:23 +0100

    Added keyBoardAction

--- app/src/main/java/com/decline/noteapp/app_features/presentation/add_edit_note/components/TransparentHintTextField.kt
+++ app/src/main/java/com/decline/noteapp/app_features/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -3,14 +3,18 @@ package com.decline.noteapp.app_features.presentation.add_edit_note.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TransparentHintTextField(
     text: String,
@@ -22,6 +26,8 @@ fun TransparentHintTextField(
     singleLine: Boolean = false,
     onFocusChange: (FocusState) -> Unit
 ) {
+
+    val keyboardController = LocalSoftwareKeyboardController.current
     Box(
         modifier = modifier
     ) {
@@ -30,6 +36,7 @@ fun TransparentHintTextField(
             onValueChange = onValueChange,
             singleLine = singleLine,
             textStyle = textStyle,
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
             modifier = Modifier
                 .fillMaxWidth()
                 .onFocusChanged {
